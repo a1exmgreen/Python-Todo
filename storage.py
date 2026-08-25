6001ea8101618ab6a3ef0ffcef48894125a92d9c
Alexander Green <166018685+a1exmgreen@users.noreply.github.com>
2026-08-25 15:26:41 +0100

    Enhance task loading and saving functionality
    Refactor load_tasks to handle completed tasks and ignore empty lines. Update save_tasks to write tasks with completion status.

--- storage.py
+++ storage.py
@@ -1,15 +1,48 @@
 TASK_FILE = "tasks.txt"
 
 def load_tasks():
+    tasks = []
+
     try:
         with open(TASK_FILE, "r", encoding="utf-8") as file:
-            tasks  = file.read().splitlines()
-            return tasks
+            for line in file:
+                line = line.strip()
+
+                # Ignore  empty lines
+                if not line:
+                    continue
+
+                # Load tasks saved in the file
+                if "|" in line:
+                    completed_value, task_test = line.split("|", 1)
+
+                    task = {
+                        "text": task_test,
+                        "completed": completed_value == "1"
+                    }
+
+                # Load older tasks as incomplete
+                else:
+                    task = {
+                        "text": line,
+                        "completed": False
+                    }
+
+                tasks.append(task)
 
     except FileNotFoundError:
-        return []
+        pass
+
+    return tasks
 
 def save_tasks(tasks):
-    with open(TASK_FILE, "w") as file:
+    with open(TASK_FILE, "w", ecoding="utf-8") as file:
         for task in tasks:
-            file.write(task + "\n")
+            if task["completed"]:
+                completed_value = "1"
+            else:
+                completed_value = "0"
+
+            file.write(
+                f"{completed_value}|{task['text']}\n"
+            )
